f159fac21a41fbaca8710b2a788cc7f96f496d37
josmeuleman <josmeuleman@gmail.com>
2023-03-16 16:30:03 +0100

    Create FRTimeDemo.ino

---
+++ examples/FRTimeDemo/FRTimeDemo.ino
@@ -0,0 +1,38 @@
+// Testscript OverSampling & Timers on the Flight Recorder PCB
+// Required hardware:
+// - 1x potmeter
+// - 6x breadboard cables female-male
+// Connections:
+// - Potmeter1, GND - Analog Input J6, GND
+// - Potmeter1, Wiper - Analog Input J6, D35
+// - Potmeter1, VCC - Analog Input J6, 3.3V
+// Required libraries:
+// - FRLib (download from https://github.com/josmeuleman/FRLib, unzipped in ../Documents/Arduino/libraries/ )
+//
+// 2023-03-14, Jos Meuleman, Inholland Aeronautical & Precision Engineering, The Netherlands
+#include <HardwareSerial.h>
+#include <TinyGPSPlus.h>
+#include <FRGeneric.h>
+
+HardwareSerial SerialGPS(2);  // The serial connection to the GPS device
+TinyGPSPlus gps;
+
+void setup() {
+  Serial.begin(9600);     // Start the serial communciation
+  SerialGPS.begin(9600);
+}
+
+void loop() {
+  long tMillis = millis();
+
+  while (SerialGPS.available() > 0) {
+    gps.encode(SerialGPS.read());
+  }
+  Serial.printf("The time since startup in milliseconds: %d; ", tMillis);
+  Serial.printf("The time of the GPS sensor: %s ;",createTimeString(gps.time.hour(), gps.time.minute(), gps.time.second()));
+  Serial.printf("Number of satellites: %d", gps.satellites.value());
+ 
+  Serial.println();
+  delay(1000);
+
+}
