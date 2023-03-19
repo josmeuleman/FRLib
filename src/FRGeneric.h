d534190790de395477429c1a7eadda721f34f185
josmeuleman <josmeuleman@gmail.com>
2023-03-19 15:36:12 +0100

    Update FRGeneric.h

--- src/FRGeneric.h
+++ src/FRGeneric.h
@@ -6,31 +6,31 @@
 #define FRGeneric_h
 #include <cstdio>
 
-String createTimeString(int hour, int minute, int second) {
+static String createTimeString(int hour, int minute, int second) {
   char buffer[11];
   sprintf(buffer, "%02d:%02d:%02d; ", hour, minute, second);
   return  buffer;
 }
 
-String createDateString(int year, int month, int day){
+static String createDateString(int year, int month, int day){
   char buffer[13];
   sprintf(buffer, "%04d-%02d-%02d; ", year, month, day);
   return  buffer;
 }
 
-String createFloatString(float value, unsigned int accuracy){
+static String createFloatString(float value, unsigned int accuracy){
   String myString = String(value, accuracy);
   myString.concat("; ");
   return myString;
 }
 
-String createIntString(int value) {
+static String createIntString(int value) {
   String myString = String(value);
   myString.concat("; ");
   return myString;  
 }
 
-float mapf(float x, float in_min, float in_max, float out_min, float out_max) {
+static float mapf(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
