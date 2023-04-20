e76fd3fb2c277b6707dfdb09d0e497b8afdca665
josmeuleman <josmeuleman@gmail.com>
2023-04-20 17:10:51 +0200

    Update FRIntegrationTemplate.ino

--- examples/FRIntegrationTemplate/FRIntegrationTemplate.ino
+++ examples/FRIntegrationTemplate/FRIntegrationTemplate.ino
@@ -138,8 +138,11 @@ void loop() {
   // Read the receiver2 and store the values
   //-------------------------------------------------------------------------------------------------------
   for (int i = 0; i < NUMBEROFCHANNELS2; i++) {
-    channelValues[i] = servoTargetPos[i];
+    channelValues[i] = receiver2.ReadChannel(i)[i];
+    Serial.print(channelValues[i]);
+    Serial.print("; ");
   }
+  Serial.println();
 
   //-------------------------------------------------------------------------------------------------------
   // Process the controller channels
